e91bb0bec73db732dd9049213c87f8f9ec052368
Ray Dixon <ray@Rays-MacBook-Air.local>
2024-01-04 15:10:21 -0500

    add timer function

--- assets/modal.js
+++ assets/modal.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   const modal = $('#modal');
   const closeButton = $('#closeButton');
   const modalAPI = $('#modalAPI');
-
+  const timer = $('#timer');
   const settings = {
     async: true,
     crossDomain: true,
@@ -14,6 +14,49 @@ $(document).ready(function() {
     }
   };
 
+  let timerInterval;
+  let timerCount = 0;
+  let minutes = 25;
+  let seconds = 0;
+  const timerElement = $('#timer-section h2');
+
+  function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(timerInterval);
+  }
+
+  function updateTimer() {
+    seconds--;
+    if (seconds < 0) {
+      minutes--;
+      seconds = 59;
+    }
+  
+    if (minutes === 0 && seconds === 0) {
+      stopTimer();
+      timerCount++;
+      
+      if (timerCount % 4 === 0) {
+        minutes = 25; // Reset to 25 minutes
+      } else {
+        minutes = 5; // Reset to 5 minutes
+      }
+  
+      // Display the modal when the 25-minute timer ends
+      if (timerCount % 4 === 1) {
+        displayModal();
+        startModalTimer();
+      }
+  
+      startTimer();
+    }
+  
+    timerElement.text(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+  }
+
   function displayModal() {
     modal.show();
   }
@@ -22,7 +65,37 @@ $(document).ready(function() {
     modal.hide();
   }
 
+  // Start the 5-minute timer in the modal
+  function startModalTimer() {
+    let timeLeft = 5 * 60; // 5 minutes in seconds
+
+    const modalTimerInterval = setInterval(function() {
+      const modalMinutes = Math.floor(timeLeft / 60);
+      const modalSeconds = timeLeft % 60;
+
+      timer.text(`${modalMinutes}:${modalSeconds.toString().padStart(2, '0')}`); // small timer that appears in the top right of the modal
+
+      if (timeLeft === 0) {
+        clearInterval(modalTimerInterval);
+        closeModal();
+        startTimer();
+      } else {
+        timeLeft--;
+      }
+    }, 1000); // Update every second
+  }
+
   closeButton.on('click', closeModal);
+
+  // Start the timer when the start button is clicked
+  $('#start').on('click', function() {
+    startTimer();
+  });
+
+  // Stop the timer when the stop button is clicked
+  $('#stop').on('click', function() {
+    stopTimer();
+  });
 
   $.ajax(settings).done(function(response) {
     const exercises = response;
@@ -30,6 +103,5 @@ $(document).ready(function() {
     const randomExercise = exercises[randomIndex];
     const instructions = randomExercise.instructions;
     modalAPI.text(instructions);
-    displayModal();
   });
 });
